c604884fe3de876df10b1ff619f9270d5b4ec33b
Jeff Layton <70778898+jefflayton@users.noreply.github.com>
2020-09-10 15:25:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function startPrompt() {
-    var name = prompt("Hello! \nWhat is your name?", "Jeff");
-	if (name != null)
-	{
-		document.getElementById("welcome").innerHTML = "Welcome " + name + "!";
-	} else {
-		document.getElementById("welcome").innerHTML = "Welcome!";
-	}
-    
-}
 
-window.onload = startPrompt;
